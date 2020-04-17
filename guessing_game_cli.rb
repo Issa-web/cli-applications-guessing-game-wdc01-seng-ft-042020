b1178e46086a815afc424547674eed60f750cd3e
Issa Sangare <issabmsangare@hotmail.com>
2020-04-17 16:03:21 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,14 @@
 # Code your solution here!
+require 'pry'
+def run_guessing_game
+  rand_number = rand(6)+1
+user_number_guess = gets.chomp
+if rand_number.to_s == user_number_guess
+  puts "You guessed the correct number! "
+elsif rand_number.to_s != user_number_guess
+  puts "Sorry! The computer guessed rand_number "
+elsif run_guessing_game == "exit"
+  puts "Goodbye!"
+end
+
+end
