c85fe8ceccae57a543003d356ff6b06a6a900ab6
Issa Sangare <issabmsangare@hotmail.com>
2020-04-17 16:21:44 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -8,8 +8,8 @@ if rand_number.to_s == user_number_guess
 elsif user_number_guess == "exit"
      puts "Goodbye!"
   #puts "Sorry! The computer guessed rand_number "
-else rand_number.to_s != user_number_guess
-  puts "Sorry! The computer guessed rand_number "
+else #rand_number.to_s != user_number_guess
+  puts "Sorry! The computer guessed #{rand_number} "
 # else run_guessing_game == "exit"
 #    puts "Goodbye!"
 end
